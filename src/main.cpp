4c309d6eaa7d553475ba8e7847a8722e1fc8c93e
Blanco <96238298+BlancoTLM@users.noreply.github.com>
2025-05-23 19:29:09 +0200

    Initial setup - Spawned particles

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include "opengl-framework/opengl-framework.hpp"
 #include "utils.hpp"
+#include <vector>
+#include <glm/glm.hpp>
+
+struct Particle {
+    glm::vec2 position;
+
+    Particle() {
+        position = glm::vec2{
+            utils::rand(-gl::window_aspect_ratio(), +gl::window_aspect_ratio()),
+            utils::rand(-1.0f, 1.0f)
+        };
+    }
+};
 
 int main()
 {
@@ -8,14 +21,18 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-    // TODO: create an array of particles
+    std::vector<Particle> particles;
+    for (int i = 0; i < 100; ++i) {
+        particles.emplace_back();
+    }
 
     while (gl::window_is_open())
     {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // TODO update particles
-        // TODO render particles
+        for (const auto& p : particles) {
+            utils::draw_disk(p.position, 0.02f, {1.0f, 0.5f, 0.2f, 1.0f}); // rayon fixe, couleur orange
+        }
     }
 }
